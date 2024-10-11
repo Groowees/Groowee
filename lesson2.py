2ee8c3c4374631664adb928819ce25ec9daaac22
Groowee <semakvalera@gmail.com>
2024-10-11 21:02:18 +0300

    Initail commit

---
+++ lesson2.py
@@ -0,0 +1,34 @@
+# # number = 10+2
+# # print(number)
+# # print(type(number))
+# # number = 20.5
+# # print(number)
+# # print(type(number))
+# # number = "Vasya"
+# # print(number)
+# # print(type(number))
+# # number = True
+# # print(number)
+# # print(type(number))
+# #
+# # #":=" моржовый оператор ( тут же создает пременную )
+# # print(number := 10)
+# # print(type(number))
+#
+# nuber2=number+2
+# print(nuber2)
+# print(6//4)
+# print(6%4)
+number = 106
+n1 = number // 100
+n2 = number // 10 % 10
+n3 = number % 10
+print(n1)
+print(n2)
+print(n3)
+result = n1 + n2 + n3
+print(result)
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(name + str(age))
+
