2da4d4f4908c47fc1be266d22003c369609bc4d5
Groowee <semakvalera@gmail.com>
2024-10-12 17:15:48 +0300

    Initail commit

--- lesson2.py
+++ lesson2.py
@@ -29,39 +29,52 @@
 # result = n1 + n2 + n3
 # print(result)
 #
+import math
 
 # name = input("Enter your name: ")
 # age = input("Enter your age: ")
 # print(name + str(age))
 #
+#
+#
+# number = int(input("Enter a number: "))
+# n1 = number // 1000
+# n2 = number % 1000 // 100
+# n3 = number % 100 //10
+# n4 = number % 10
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+#
+#
+# # result = n1 + n2 + n3 + n4
+# # # print(result)
+#
+#
+# #  12345
+# number = int(input("Enter a number: "))
+# n1 = number % 10
+# n2 = number % 100 // 10
+# n3 = number % 1000 // 100
+# n4 = number % 10000 // 1000
+# n5 = number // 10000
+# print(n1)
+# print(n2)
+# print(n3)
+# print(n4)
+# print(n5)
+# result = n1 * 10000 + n2 * 1000 + n3 * 100 + n4 * 10 + n5
+# print(result)
+
+a1 = int(input("Enter a nomer1: "))
+b2 = int(input("Enter a nomer2: "))
+# c3 = int(input("Enter a nomer3: "))
+res_c = math.sqrt((a1 ** 2) + (b2 ** 2))
+# res_a = math.sqrt((c3 ** 2) - (b2 ** 2))
+# # res_b = math.sqrt((c3 ** 2) - (a1 ** 2))
+# print(res_a)
+# print(res_b)
+print(res_c)
 
 
-number = int(input("Enter a number: "))
-n1 = number // 1000
-n2 = number % 1000 // 100
-n3 = number % 100 //10
-n4 = number % 10
-print(n1)
-print(n2)
-print(n3)
-print(n4)
-
-
-# result = n1 + n2 + n3 + n4
-# # print(result)
-
-
-#  12345
-number = int(input("Enter a number: "))
-n1 = number % 10
-n2 = number % 100 // 10
-n3 = number % 1000 // 100
-n4 = number % 10000 // 1000
-n5 = number // 10000
-print(n1)
-print(n2)
-print(n3)
-print(n4)
-print(n5)
-result = n1 * 10000 + n2 * 1000 + n3 * 100 + n4 * 10 + n5
-print(result)
